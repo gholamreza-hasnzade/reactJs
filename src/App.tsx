e442254e09ceb10ebfbe00f237e8d258d46fe0a0
farimahfarahani <Farimahfarahani@gmail.com>
2025-01-17 06:51:22 -0800

    pull data

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,15 @@
-// src/App.tsx
-import React, { useEffect, useState } from 'react';
-import axios from "axios"
-/* import Button from './Button';
-import { FaBeer, FaCoffee } from 'react-icons/fa';  */// Example icons from react-icons
+import React from 'react'
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Products from './components/views/products'
 
-const App: React.FC = () => {
-  /*   const [loading, setLoading] = useState(false);
-  
-    const handleClick = (variant: string, color: string) => {
-      alert(`${color} ${variant} button clicked!`);
-    };
-  
-    const handleLoadingClick = () => {
-      setLoading(true);
-      setTimeout(() => {
-        setLoading(false);
-        alert('Loading finished!');
-      }, 2000); // Simulate a loading delay
-    }; */
-  const [forms, setForms] = useState({
-    datas: [],
-    detailData: {}
-  });
-
-  const { datas } = forms;
-  console.log(datas);
-  
-  useEffect(() => {
-    const fetchDatas = async () => {
-      try {
-        const { status, data } = await axios.get(`https://api.escuelajs.co/api/v1/products`)
-        if (status === 200) {
-          setForms((prev) => ({
-            ...prev,
-            datas: data,
-          }));
-        }
-      } catch (error) {
-        console.log(error);
-
-      }
-
-    }
-
-    fetchDatas()
-  }, [])
+const App = () => {
   return (
-    <div className="">
+    <Router>
+    <Routes>
+      <Route element={<Products />} path='/' />
+    </Routes>
+    </Router>
+  )
+}
 
-    </div>
-  );
-};
-
-export default App;
+export default App
